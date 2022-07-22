90fb2fd5b74b4b6d4ca11351ddc4c849a4c6db67
cfarn <58476975+cfarn@users.noreply.github.com>
2022-07-22 12:02:18 +0200

    addTask

--- src/App.js
+++ src/App.js
@@ -5,20 +5,33 @@ class App extends React.Component {
   constructor () {
     super ()
 
-    // this.state = {
-    // tasks: []
-    // }
+    this.state = {
+    tasks: []
+    }
   }
 
-  
+  addTask(str) {
+    this.setState({
+      tasks: [{
+        description: "Description de ma tache", 
+        status: "To do",
+      }]
+    })
+    console.log(str)
+  }
 
 
 
   render() {
     return(
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <div className='container'>
+        <h1 className="text-3xl font-bold underline">ToDo List</h1>
+        <div>
+          <Form
+            addTask={this.addTask}
+          />
+        </div>
+      </div>
     )
   }
 }
