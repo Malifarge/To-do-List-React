313909e690e0d090c666db7dd1d225c194505ae2
Thomas <malifarge.thomas9@gmail.com>
2022-07-22 10:45:36 +0200

    form

---
+++ src/compnents/Form.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Form extends React.Component{
+    render() {
+        return (
+            <article>
+                <input type="text" />
+                <button type="submit">Add Task</button>
+            </article>
+            
+        );
+    }
+}
+
+export default Form
