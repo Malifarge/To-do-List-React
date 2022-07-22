7555911c7877932653df426f5db0e2aa962497e5
Thomas <malifarge.thomas9@gmail.com>
2022-07-22 17:18:48 +0200

    filter start

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ class App extends React.Component {
     super ()
 
     this.state = {
-    tasks: []
+    tasks: [],
+    Todo: [],
+    Doing: [],
+    Done: [],
+    Tab: "All"
     }
   }
 
@@ -18,6 +22,7 @@ class App extends React.Component {
     this.setState({
       tasks: newTasks
     })
+    
   }
 
   deleteTask = (i) => {
@@ -28,21 +33,51 @@ class App extends React.Component {
     })
   }
 
+  handleClickFilter = (filtre) =>{
+    this.setState({
+      Tab:filtre
+    })
+  }
+
 
   render() {
     return(
       <div className='container p-6'>
         <h1 className="text-3xl font-bold underline">ToDo List</h1>
+
         <div className='my-5'> 
           <Form
             addTask={this.addTask}
           />
         </div>
+
+        <div className='flex gap-5'>
+        <button className='border border solid border-black p-1 rounded-lg w-24' onClick={() => this.handleClickFilter("All")}>All</button>
+        <button className='border border solid border-black p-1 rounded-lg w-24' onClick={() => this.handleClickFilter("To do")}>To do</button>
+        <button className='border border solid border-black p-1 rounded-lg w-24' onClick={() => this.handleClickFilter("Doing")}>Doing</button>
+        <button className='border border solid border-black p-1 rounded-lg w-24' onClick={() => this.handleClickFilter("Done")}>Done</button>
+        </div>
+
         <div className='shadow-md'>
-          <List
+          {this.state.Tab==="All" && <List
             deleteTask={this.deleteTask}
             tasks={this.state.tasks}
-          />
+          />}
+
+          {this.state.Tab==="To do" && <List
+            deleteTask={this.deleteTask}
+            tasks={this.state.Todo}
+          />}
+
+          {this.state.Tab==="Doing" && <List
+            deleteTask={this.deleteTask}
+            tasks={this.state.Doing}
+          />}
+
+          {this.state.Tab==="Done" && <List
+            deleteTask={this.deleteTask}
+            tasks={this.state.Done}
+          />}
         </div>
       </div>
     )
