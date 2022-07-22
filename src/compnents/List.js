b3756309e836c525e0291ec3c364be6bc34b259a
Thomas <malifarge.thomas9@gmail.com>
2022-07-22 14:44:45 +0200

    list

--- src/compnents/List.js
+++ src/compnents/List.js
@@ -1,9 +1,17 @@
-// import React from "react";
+import React from "react";
 
-// class List extends React.Component{
-//    render() {
-//     console.log(this.props.tasks);
-//    }
-// }
+class List extends React.Component{
+   render() {
+    console.log(this.props.tasks);
+    this.props.tasks.map((task)=>{
+        return(
+        <li>
+        <p>{task}</p>
+        <button onClick={() => deleteTask(i)}>Delete</button>
+        </li>
+        )
+    })
+   }
+}
 
-// export default List
+export default List
